68d1a4208835695179d9af55037b210d67d55e1c
m4riduda <maria2.vieira@alunos.ifsuldeminas.edu.br>
2026-04-17 10:09:35 -0300

    Create Remedios.java

---
+++ Hospital/src/main/java/br/edu/ifsuldeminas/hospital/modelo/entidade/Remedios.java
@@ -0,0 +1,72 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.edu.ifsuldeminas.hospital.modelo.entidade;
+
+/**
+ *
+ * @author 13702083677
+ */
+public class Remedios {
+    private int codRemedio;
+    private String nomeRemedio;
+    private String laboratorio;
+    private String efeitosColaterais;
+
+    public int getCodRemedio() {
+        return codRemedio;
+    }
+
+    public void setCodRemedio(int codRemedio) {
+        this.codRemedio = codRemedio;
+    }
+
+    public String getNomeRemedio() {
+        return nomeRemedio;
+    }
+
+    public void setNomeRemedio(String nomeRemedio) {
+        this.nomeRemedio = nomeRemedio;
+    }
+
+    public String getLaboratorio() {
+        return laboratorio;
+    }
+
+    public void setLaboratorio(String laboratorio) {
+        this.laboratorio = laboratorio;
+    }
+
+    public String getEfeitosColaterais() {
+        return efeitosColaterais;
+    }
+
+    public void setEfeitosColaterais(String efeitosColaterais) {
+        this.efeitosColaterais = efeitosColaterais;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 37 * hash + this.codRemedio;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Remedios other = (Remedios) obj;
+        return this.codRemedio == other.codRemedio;
+    }
+    
+
+}
