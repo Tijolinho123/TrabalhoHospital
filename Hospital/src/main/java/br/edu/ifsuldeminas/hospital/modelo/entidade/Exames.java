e84bdf8f22aa0c6a3ec8caeced100c5e40d767dc
m4riduda <maria2.vieira@alunos.ifsuldeminas.edu.br>
2026-04-17 10:14:18 -0300

    Create Exames.java

---
+++ Hospital/src/main/java/br/edu/ifsuldeminas/hospital/modelo/entidade/Exames.java
@@ -0,0 +1,81 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.edu.ifsuldeminas.hospital.modelo.entidade;
+
+/**
+ *
+ * @author 13702083677
+ */
+public class Exames {
+    private int codExame;
+    private String nomeExame;
+    private String descricao;
+    private double valor;
+    private String preparo;
+
+    public int getCodExame() {
+        return codExame;
+    }
+
+    public void setCodExame(int codExame) {
+        this.codExame = codExame;
+    }
+
+    public String getNomeExame() {
+        return nomeExame;
+    }
+
+    public void setNomeExame(String nomeExame) {
+        this.nomeExame = nomeExame;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+    public String getPreparo() {
+        return preparo;
+    }
+
+    public void setPreparo(String preparo) {
+        this.preparo = preparo;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 83 * hash + this.codExame;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Exames other = (Exames) obj;
+        return this.codExame == other.codExame;
+    }
+    
+    
+}
